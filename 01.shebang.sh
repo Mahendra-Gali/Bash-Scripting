22e4e5b4c1f0b6724333744de7be2b131bf4e5b7
Mahendra Gali <gmahendra1601@gmail.com>
2025-05-01 16:15:32 +0530

    changing line in shebang script

--- 01.shebang.sh
+++ 01.shebang.sh
@@ -3,6 +3,6 @@ echo "Hello, World!"
 sleep 1
 echo "This is a simple bash script."
 sleep 2
-echo " using bash we will be automating manual effort."
+echo "using bash we will be automating manual effort."
 sleep 3     
 echo "script name: $# completed"
