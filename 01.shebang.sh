d5e2e28bda3da71aa9b59d1493bd30c15f855955
Mahendra Gali <gmahendra1601@gmail.com>
2025-05-01 16:13:30 +0530

    Adding some sample line in shebang script

--- 01.shebang.sh
+++ 01.shebang.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Hello, World!"
+sleep 1
 echo "This is a simple bash script."
+sleep 2
 echo " using bash we will be automating manual effort."
+sleep 3     
 echo "script name: $# completed"
